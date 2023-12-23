fd35a5b90a6503a0cd3c62a8005ce341b7b87fa0
taehee-kim-dev <taehee.kim.dev@gmail.com>
2023-12-23 12:07:45 +0900

    env: initial project settings

---
+++ src/test/java/taeheekim/springbatchpractice/SpringBatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package taeheekim.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
